c87ae7b371b09a94f63ab6f879297e88844d367e
Ethan Ryan <ethanryan@gmail.com>
2017-02-21 14:16:10 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  i = 0 #counter variable i set to 0
+  while i < array.length #start while loop execute code below as long as i is less than the length of the array
+    yield array[i] #grab the value of each successive index element and yield it to a block
+    i = i + 1 #increment value of i variable
+  end #end while loop
+  array #return original array
+end #end method
